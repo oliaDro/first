9d5a4cb765edc5937e1536272dbe552ad04d0e63
sdrozdiuk <sdrozdiuk12@gmail.com>
2023-10-09 22:40:02 +0200

    first commit

---
+++ Examen/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Examen
+{
+	internal class Program
+	{
+		public static BlockingCollection<int> Numbers { get; set; } = new BlockingCollection<int>();
+
+		public static Random Random { get; set; } = new Random();
+
+		static void Main(string[] args)
+		{
+			Task.Run(() => { Console.WriteLine("test thread"); });
+			Run();
+		}
+
+		public static async Task Run()
+		{
+			await Console.Out.WriteLineAsync($"Task run in {Task.CurrentId}, start program");
+			await TestFill();
+			Task.Run(() => FindMax());
+			Task.Run(() => FindMin());
+			Task.Run(() => FindAvg());
+			Console.ReadLine();
+		}
+
+		public static async Task TestFill()
+		{
+			await Console.Out.WriteLineAsync($"Task run in {Task.CurrentId}");
+
+			for (int i = 0; i <= 1000; i++)
+			{
+				Numbers.Add(Random.Next(0, 5000));
+			}
+
+			await Console.Out.WriteLineAsync($"Fill finished ");
+		}
+
+		public static void FindMax()
+		{
+			Console.WriteLine($"Task findmax is {Task.CurrentId}");
+			var max = Numbers.Max();
+			Console.WriteLine($"Max is {max}");
+		}
+		public static void FindMin()
+		{
+			Console.WriteLine($"Task findmin is {Task.CurrentId}");
+			var min = Numbers.Min();
+			Console.WriteLine($"Min is {min}");
+		}
+
+		public static void FindAvg()
+		{
+			Console.WriteLine($"Task findAvg is {Task.CurrentId}");
+			var avg = Numbers.Average();
+			Console.WriteLine($"Average is {avg}");
+		}
+
+	}
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+	class Program
+	{
+		static void Factorial(int x)
+		{
+			Console.WriteLine($"Star calculation faxtorial {x} ");
+			int result = 1;
+			for (int i = 1; i <= x; i++)
+				result *= i;
+			Thread.Sleep(3000);
+			Console.WriteLine($"Finish calculation factorial {x}.");
+			Thread.Sleep(3000);
+			Console.WriteLine($"Result { result}");
+		}
+
+		static void Main(string[] args)
+		{
+			Parallel.Invoke(() => Factorial(10),
+						 () => Console.WriteLine("_______________________________")
+						 );
+		}
+	}
+}
